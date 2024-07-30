ed9bd30351e5fec89d02e52e53cae4fc194eb0e2
JSand89 <javier.esand@gmail.com>
2024-07-30 16:38:17 -0500

    resolviendo el problema del JSON

--- rosterData.py
+++ rosterData.py
@@ -28,6 +28,7 @@ CREATE TABLE Member (
 
 ''')
 handle = open('dataRoster.csv')
+f = open('user.txt','w') #con este comando crean o abren el archivo
 
 for line in handle:
     line = line.strip()
@@ -50,6 +51,11 @@ for line in handle:
     cur.execute('SELECT user_id FROM User WHERE name = ? ', (name, ))
     user_id = cur.fetchone()[0]
 
+    text =f'["user_id":{user_id},"name":"{name},"]' # con esta funcion crean el sting
+    text = text.replace("[","{") # con esto remplazan para tener los parentesis adecuados
+    text = text.replace("]","}")
+    f.write(text) # en esta linea escriben el texto, tienen que repetir la misma logica con las 3 bases de datos, usen archivos diferentes y despues los unen 
+
     cur.execute('''INSERT OR IGNORE INTO Course (title) 
         VALUES ( ? )''', ( title, ) )
     cur.execute('SELECT course_id FROM Course WHERE title = ? ', (title, ))
@@ -60,6 +66,9 @@ for line in handle:
 
 
     conn.commit()
+
+
+f.close()  
 
 
     
